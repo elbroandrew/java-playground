a12b5c9e995aaa29164d381d7d674538ff39dc6a
Andrew <elbroandrew@gmail.com>
2021-09-27 15:56:17 +1000

    feature: add parse double solution

--- src/javarush/ParseDoubleSolution.java
+++ src/javarush/ParseDoubleSolution.java
@@ -2,7 +2,13 @@ package javarush;
 
 public class ParseDoubleSolution
 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        String string = "12.84";
+        double x = Double.parseDouble(string);
+        int rounded = (int) Math.round(x);
+
+        System.out.println(rounded);
 
     }
 }
