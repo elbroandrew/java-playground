6a85ad7bdc271f8c44528cc2b31b986ecfafea17
elbroandrew <elbroandrew@gmail.com>
2022-11-24 16:07:47 +1000

    add join

--- src/Synchronized/Test.java
+++ src/Synchronized/Test.java
@@ -3,12 +3,12 @@ package Synchronized;
 public class Test {
     private int counter;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Test test = new Test();
         test.doWork();
     }
 
-    public void doWork(){
+    public void doWork() throws InterruptedException {
         Thread t1 = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -29,6 +29,8 @@ public class Test {
 
         t1.start();
         t2.start();
+        t1.join();
+        t2.join();
 
         System.out.println(counter);
     }
