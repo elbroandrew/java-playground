dd252892c45ea44b6560a142185575be7d7aa1a4
Andrew <elbroandrew@gmail.com>
2023-12-29 22:27:25 +1000

    write bytes to file

--- src/main/java/bytes/MainApp.java
+++ src/main/java/bytes/MainApp.java
@@ -1,25 +1,16 @@
 package bytes;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+
+import java.util.ArrayList;
 
 public class MainApp {
 
     public static void main(String[] args) {
-        byte[] buffer = new byte[12];
 
-        try (
-                FileInputStream in = new FileInputStream("src/main/java/bytes/input.txt");
-                FileOutputStream out = new FileOutputStream("src/main/java/bytes/out.txt");
-        )  {
-            int n = in.read(buffer);
-            out.write(buffer, 0, n);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(5);
+        arrayList.add(1);
+        System.out.println(arrayList);
 
     }
 }
