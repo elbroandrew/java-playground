068eacc247f7f3acb2728b29b6aae8144ca1e1de
Andrew <elbroandrew@gmail.com>
2025-05-11 18:00:34 +1000

    remove value from linked list

---
+++ src/main/java/RemoveFromLinkedList.java
@@ -0,0 +1,33 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class RemoveFromLinkedList {
+
+//    LinkedList<Integer> ln = new LinkedList<>(List.of(1, 2, 6, 3, 4, 5, 6));
+//    LinkedList<Integer> ln2 = new LinkedList<>();
+//
+//        for (Integer i : ln) {
+//        if (i != 6) {
+//            ln2.add(i);
+//        }
+//    }
+//        System.out.println(ln2);
+//
+//    // SAme with 'Iterator'
+//    LinkedList<Integer> ln3 = new LinkedList<>(List.of(1, 2, 6, 3, 4, 5, 6));
+//
+//    Iterator<Integer> iterator = ln3.iterator();
+//        while (iterator.hasNext()) {
+//        if (iterator.next() == 6) {
+//            iterator.remove();  // Safely removes the current element
+//        }
+//    }
+//
+//    //Same with 'removeIf'
+//    LinkedList<Integer> ln4 = new LinkedList<>(List.of(1, 2, 6, 3, 4, 5, 6));
+//        ln4.removeIf(i -> i == 6);  // Removes all 6s in one line
+//
+//        System.out.println(ln4);  // [1, 2, 3, 4, 5]
+//}
+}
