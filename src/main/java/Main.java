24d85647f680eb9f0ed5adfcaa04c5d4569f83bc
Andrew <elbroandrew@gmail.com>
2025-05-15 10:52:48 +1000

    threads

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,72 +1,56 @@
-import java.lang.Thread.State;
+
 
 public static void main(String[] args) throws InterruptedException {
 
-
-    BlockingQueue<String> drop  = new ArrayBlockingQueue<String>(1, true);
-
-    Thread t1 = new Thread(new Producer(drop));
-    Thread t2 = new Thread(new Consumer(drop));
+    StopRun stopRun = new StopRun();
+    Thread t1 = new Thread(stopRun, "TEST");
+    t1.setDaemon(true);
     t1.start();
-    t2.start();
-
-    //Monitor thread states
-    while (true) {
-
-        Thread.sleep(500);
-        Thread.State prodState = t1.getState();
-        Thread.State consState = t2.getState();
-
-        System.out.println(prodState + ", " + consState);
+    System.out.println(t1.threadId());
+    try {
+        Thread.sleep(1000);
+    } catch (InterruptedException e) {
+        throw new RuntimeException(e);
     }
 
+    System.out.println("requesting stop");
+    stopRun.requestStop();
+    System.out.println("stop requested");
 
-//    System.out.println("finished");
+//    t1.join();
 
 }
 
 
-public static class Producer implements Runnable{
+public static class StopRun implements Runnable{
 
-    private BlockingQueue<String> drop;
-    public Producer(BlockingQueue<String> drop){
-        this.drop = drop;
+    private boolean stopped = false;
+
+    public synchronized void requestStop(){
+        this.stopped = true;
+
+    }
+
+    public synchronized boolean isStopRequested(){
+        return this.stopped;
     }
 
     @Override
     public void run() {
-        System.out.println("from runnable class");
-        for (int i = 0; i < 5; i++) {
+        while (!isStopRequested()){
             try {
-                drop.put(String.format("%s", i));
+                Thread.sleep(500);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
-                return;
+                e.printStackTrace();
             }
         }
+
     }
 }
 
-public static class Consumer implements Runnable{
 
-    BlockingQueue<String> drop;
-    public Consumer(BlockingQueue<String> drop){
-        this.drop = drop;
-    }
 
-    @Override
-    public void run() {
-        System.out.println("from runnable class");
-        while (true){
-            try {
-                drop.take();
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                return;
-            }
-        }
-    }
-}
 
 
 
