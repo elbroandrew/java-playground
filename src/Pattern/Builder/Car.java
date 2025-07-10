6c239e7cc38558721ef12c7b08dac23fdc324af2
Andrew <elbroandrew@gmail.com>
2025-07-11 00:53:23 +1000

    Builder pattern

---
+++ src/Pattern/Builder/Car.java
@@ -0,0 +1,47 @@
+package Pattern.Builder;
+
+public class Car {
+
+    String maker;
+    String color;
+    String owner;
+
+    private Car(String maker, String color, String owner){
+        this.maker = maker;
+        this.color = color;
+        this.owner = owner;
+    }
+
+    public void displayInfo(){
+        System.out.println(maker + " " + color + " " + owner);
+    }
+
+    static class CarBuilder{
+
+        // Create default fields
+        private String maker = "none";
+        private String color = "none";
+        private String owner = "none";
+
+        //methods that return CarBuilder
+
+        public CarBuilder withMaker(String maker){
+            this.maker = maker;
+            return this;
+        }
+
+        public CarBuilder withColor(String color){
+            this.color = color;
+            return this;
+        }
+
+        public CarBuilder withOwner(String owner){
+            this.owner = owner;
+            return this;
+        }
+
+        public Car build(){
+            return new Car(maker, color, owner);
+        }
+    }
+}
---
+++ src/Pattern/Builder/Main.java
@@ -0,0 +1,21 @@
+package Pattern.Builder;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Car defaultCar = new Car.CarBuilder().build();
+        defaultCar.displayInfo();
+
+        Car redCar = new Car.CarBuilder()
+                .withColor("red")
+                .build();
+        redCar.displayInfo();
+
+        Car johnRedCar = new Car.CarBuilder()
+                .withColor("red")
+                .withOwner("John")
+                .build();
+        johnRedCar.displayInfo();
+    }
+}
