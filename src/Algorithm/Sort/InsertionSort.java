25c672be6d965de0a8cb2dc0300a7283e2d5cb93
Andrew <elbroandrew@gmail.com>
2025-07-18 20:45:57 +1000

    add: Insertion Sort

---
+++ src/Algorithm/Sort/InsertionSort.java
@@ -0,0 +1,25 @@
+package Algorithm.Sort;
+
+import java.util.Arrays;
+
+//Будто карты в руке сортирую. Беру одну с начала и сравниваю с оставшимися.
+public class InsertionSort {
+
+    public static void Do(int[] arr){
+
+        for (int i = 1; i < arr.length; i++) { //перемещение по несортированной части справа-налево
+            var key = arr[i];
+            int j;
+            for (j = i - 1; j >= 0; j--) { // перемещение по сортированной части слева-направо
+
+                if(key < arr[j]){
+                    arr[j+1]=arr[j];
+                }else{
+                    break;
+                }
+            }
+            arr[j+1] = key;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
--- src/Algorithm/Sort/Main.java
+++ src/Algorithm/Sort/Main.java
@@ -3,6 +3,6 @@ package Algorithm.Sort;
 public class Main {
 
     public static void main(String[] args) {
-        BubbleSort.Do(new int[]{2, 9, 1, 4});
+        InsertionSort.Do(new int[]{2, 9, 1, 4});
     }
 }
