54c86454dc41c33363bd4f1bcce27d4404499829
Andrew <elbroandrew@gmail.com>
2025-07-18 19:47:26 +1000

    add: bubble sort

---
+++ src/Algorithm/Sort/BubbleSort.java
@@ -0,0 +1,21 @@
+package Algorithm.Sort;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    public static void Do(int[] arr){
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                if(arr[j] > arr[j+1]){
+                    //Swap
+                    var tmp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1]= tmp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
---
+++ src/Algorithm/Sort/Main.java
@@ -0,0 +1,8 @@
+package Algorithm.Sort;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BubbleSort.Do(new int[]{2, 9, 1, 4});
+    }
+}
