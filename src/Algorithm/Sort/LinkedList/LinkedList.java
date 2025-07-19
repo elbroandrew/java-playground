9107f3dcf5f1aa334bbc85dad13cef239fafe9e2
Andrew <elbroandrew@gmail.com>
2025-07-19 23:04:14 +1000

    add: Sort Linked List

--- src/Algorithm/Sort/LinkedList/LinkedList.java
+++ src/Algorithm/Sort/LinkedList/LinkedList.java
@@ -32,6 +32,37 @@ public class LinkedList {
 
     }
 
+    public void sort(){
+        //insertion sort will help
+        Node currentNode = head;  //не currentNode = head.next ; sortedHead = head; т.к. это объекты, и в, случае [10(A), 5, 10(B), 3]
+        Node sortedHead = null;   // у меня получилось бы [3, 5, 10(B), 10(A)] - т.е. если есть два объекта с разными значениями, второй не будет проверяться
+
+
+        while (currentNode != null){
+            Node nextNode = currentNode.next;
+            if (sortedHead == null || currentNode.value < sortedHead.value) { // вставка в начало
+                currentNode.next = sortedHead;
+                sortedHead = currentNode;
+            }else{
+                Node sortedCurrentNode = sortedHead;
+                Node prevSortedNode = null;
+                //двигаемся по сортированной части
+                while (sortedCurrentNode != null && sortedCurrentNode.value < currentNode.value){
+                    prevSortedNode = sortedCurrentNode;
+                    sortedCurrentNode = sortedCurrentNode.next;
+                }
+                //вставка между prev и currentNode
+                prevSortedNode.next = currentNode;
+                currentNode.next = sortedCurrentNode;
+
+            }
+
+            currentNode = nextNode;
+
+        }
+        head = sortedHead;
+    }
+
     class Node{
         int value;
         Node next;
@@ -40,4 +71,5 @@ public class LinkedList {
             this.next = null;
         }
     }
+
 }
--- src/Algorithm/Sort/Main.java
+++ src/Algorithm/Sort/Main.java
@@ -8,7 +8,20 @@ public class Main {
 //        BubbleSort.Do(new int[]{1,2,3});
 
         LinkedList list = new LinkedList();
+        list.add(5);
+        list.add(9);
         list.add(1);
+        list.add(33);
+        list.add(3);
+        list.add(36);
+        list.add(33);
+        list.add(673);
+        list.add(325);
+        list.add(383);
+        System.out.println("BEFORE SORTING");
+        list.printList();
+        list.sort();
+        System.out.println("AFTER SORTING");
         list.printList();
     }
 }
