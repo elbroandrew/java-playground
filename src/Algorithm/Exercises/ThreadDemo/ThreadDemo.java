3882ac17e4b14629cde4d39cb57dec713dbc3874
Andrew <elbroandrew@gmail.com>
2025-07-24 22:40:19 +1100

    add: Multiple Threads join

--- src/Algorithm/Exercises/ThreadDemo/ThreadDemo.java
+++ src/Algorithm/Exercises/ThreadDemo/ThreadDemo.java
@@ -6,19 +6,26 @@ public class ThreadDemo {
         NewThread nt1 = new NewThread("thread-1");
         NewThread nt2 = new NewThread("thread-2");
         NewThread nt3 = new NewThread("thread-3");
-        NewThread nt4 = new NewThread("thread-4");
         nt1.thread.start();
         nt2.thread.start();
         nt3.thread.start();
-        nt4.thread.start();
+
+        System.out.println(nt1.name + " is running: " + nt1.thread.isAlive());
+        System.out.println(nt2.name + " is running: " + nt2.thread.isAlive());
+        System.out.println(nt3.name + " is running: " + nt3.thread.isAlive());
 
         try {
 
-            Thread.sleep(10000);
+            nt1.thread.join();
+            nt2.thread.join();
+            nt3.thread.join();
 
         }catch (InterruptedException e){
             System.out.println("Main thread interrupted.");
         }
+        System.out.println(nt1.name + " is still running " + nt1.thread.isAlive());
+        System.out.println(nt2.name + " is still running " + nt2.thread.isAlive());
+        System.out.println(nt3.name + " is still running " + nt3.thread.isAlive());
         System.out.println("Finishing the main thread.");
     }
 }
